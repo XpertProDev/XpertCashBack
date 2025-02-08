6618165bc421927f6e138199799236c6de570258
apple <xpertproinformatique@gmail.com>
2025-02-08 10:52:00 -0800

    first commit

---
+++ src/main/java/com/xpertcash/XpertcashApplication.java
@@ -0,0 +1,13 @@
+package com.xpertcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpertcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpertcashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpertcash/XpertcashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpertcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpertcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
